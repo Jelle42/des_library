8a03464534de4fe5cb6fe50e62a71c7035b859db
Jelle42 <jelle.roessink@hotmail.com>
2026-04-21 08:07:05 +0200

    made file for assignment, renamed repository to des_library and added .gitignore

---
+++ Assignment1/solution.py
@@ -0,0 +1,12 @@
+
+from __future__ import annotations
+
+import bisect
+import random
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+
+from des_library import Simulation, Event, TimeWeightedStatistic, SampleStatistic
+
