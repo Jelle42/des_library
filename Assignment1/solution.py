03a5973255ecc6c32cb16c7f129ac76f8d33fb0d
Jelle42 <jelle.roessink@hotmail.com>
2026-04-21 12:31:30 +0200

    added some intitial classes

--- Assignment1/solution.py
+++ Assignment1/solution.py
@@ -10,3 +10,20 @@ sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."
 
 from des_library import Simulation, Event, TimeWeightedStatistic, SampleStatistic
 
+class Vehicle:
+    def __init__(self, battery_level: float, arrival_time: float, patience_threshold: float):
+        self.remaining = 60 * (1 - battery_level)
+        self.arrival_time = arrival_time
+
+class ChargingStationModel:
+    def __init__(self):
+
+        self.sim = Simulation()
+
+class Arrival(Event):
+    def __init__(self, time):
+        super().__init__(time)
+
+class Departure(Event):
+    def __init__(self, time):
+        super().__init__(time)
