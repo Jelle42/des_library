53801a48659b650e9e988681a64145fc27048759
vincent <vincentscheinhardt@gmail.com>
2026-05-04 17:26:49 +0200

    Small fix

--- Assignment2/solution.py
+++ Assignment2/solution.py
@@ -85,7 +85,7 @@ class TransactionArrival(Event):
         sim.schedule(expiry)
 
         next_arrival = random.expovariate(m.transaction_arrival_rate)
-        sim.schedule(TransactionArrival(next_arrival, m))
+        sim.schedule(TransactionArrival(self.time + next_arrival, m))
 
 class Expire(Event):
     def __init__(self, time: float, model: GasMarket, transaction: Transaction) -> None:
