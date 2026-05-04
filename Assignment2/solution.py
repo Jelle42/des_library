055e784efe41fae20da9e8dd11dac8e45702fc44
vincent <vincentscheinhardt@gmail.com>
2026-05-04 15:35:09 +0200

    nieuw document

---
+++ Assignment2/solution.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import math
+import random
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+
+from des_library import Simulation, Event, TimeWeightedStatistic, SampleStatistic, Counter
