bca197de08aef9be16c1406d00ec7ff2db37e416
Jelle42 <jelle.roessink@hotmail.com>
2026-05-04 16:14:58 +0200

    blabla

--- Assignment2/solution.py
+++ Assignment2/solution.py
@@ -8,3 +8,9 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
 
 from des_library import Simulation, Event, TimeWeightedStatistic, SampleStatistic, Counter
+
+class GasMarket:
+    def __init__(self, gas_target: float, gas_limit: float, seed: int = 42):
+        random.seed(seed)
+        self.gas_target = gas_target
+        self.gas_limit = gas_limit
